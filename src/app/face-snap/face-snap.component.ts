f32288a9f5a7bce5e0fcb73f67baffd351d5693c
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2025-02-21 21:29:10 +0100

    Affichez des données

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  public title!: string;
+  public description!: string;
+  public createdAt!: Date;
+  public snaps!: number;
+  public imageUrl!: string;
 
+  ngOnInit() {
+    this.title = 'Beyoncé Renaissance World Tour';
+    this.description = 'The best concert of my life !';
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'https://pictures.laprovence.com/cdn-cgi/image/width=1080,format=auto,quality=80,trim.left=0,trim.top=79,trim.height=675,trim.width=1200/media/2023/05/27/20230526_RWT_Paris_Poole_MP1_3412_V1_TP.jpg'
+  }
 }
