574759513191436680de4d8f6eb23fe604f5a682
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2025-02-22 11:39:45 +0100

    Ajoutez du style dynamiquement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
         'Beyoncé Renaissance World Tour',
         'The best concert of my life !',
         new Date(),
-        21,
+        150,
         'https://pictures.laprovence.com/cdn-cgi/image/width=1080,format=auto,quality=80,trim.left=0,trim.top=79,trim.height=675,trim.width=1200/media/2023/05/27/20230526_RWT_Paris_Poole_MP1_3412_V1_TP.jpg'
       ),
       new FaceSnap(
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
         'At the waterpool',
         'I learned swim crawl !',
         new Date(),
-        11,
+        90,
         'https://www.mairie-ozoir-la-ferriere.fr/wp-content/uploads/2022/01/Piscine-25-scaled.jpg'
       )
     ]
