b64792c82b597965e2d36bdd0595c0c461c9334f
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2025-02-22 11:32:53 +0100

    Affichez des listes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,35 +10,33 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  public concert!: FaceSnap;
-  public parc!: FaceSnap;
-  public piscine!: FaceSnap;
+  public faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.concert = new FaceSnap(
-      'Beyoncé Renaissance World Tour',
-      'The best concert of my life !',
-      new Date(),
-      21,
-      'https://pictures.laprovence.com/cdn-cgi/image/width=1080,format=auto,quality=80,trim.left=0,trim.top=79,trim.height=675,trim.width=1200/media/2023/05/27/20230526_RWT_Paris_Poole_MP1_3412_V1_TP.jpg'
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        'Beyoncé Renaissance World Tour',
+        'The best concert of my life !',
+        new Date(),
+        21,
+        'https://pictures.laprovence.com/cdn-cgi/image/width=1080,format=auto,quality=80,trim.left=0,trim.top=79,trim.height=675,trim.width=1200/media/2023/05/27/20230526_RWT_Paris_Poole_MP1_3412_V1_TP.jpg'
+      ),
+      new FaceSnap(
+        'DisneyLand Paris',
+        'I met Mickey !',
+        new Date(),
+        5,
+        'https://a.travel-assets.com/findyours-php/viewfinder/images/res70/495000/495696-disneylandr-paris.jpg'
+      ),
+      new FaceSnap(
+        'At the waterpool',
+        'I learned swim crawl !',
+        new Date(),
+        11,
+        'https://www.mairie-ozoir-la-ferriere.fr/wp-content/uploads/2022/01/Piscine-25-scaled.jpg'
+      )
+    ]
 
-    this.parc = new FaceSnap(
-      'DisneyLand Paris',
-      'I met Mickey !',
-      new Date(),
-      5,
-      'https://a.travel-assets.com/findyours-php/viewfinder/images/res70/495000/495696-disneylandr-paris.jpg'
-    );
-
-    this.piscine = new FaceSnap(
-      'At the waterpool',
-      'I learned swim crawl !',
-      new Date(),
-      11,
-      'https://www.mairie-ozoir-la-ferriere.fr/wp-content/uploads/2022/01/Piscine-25-scaled.jpg'
-    );
-
-    this.parc.setLocation('Paris');
+    this.faceSnaps[1].setLocation('Paris');
   }
 }
