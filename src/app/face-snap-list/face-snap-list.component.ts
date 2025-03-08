3226256363a9e9e03c5ed6eb658b3e49c42e08c0
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2025-03-08 19:33:04 +0100

    Évitez les fuites de mémoire avec des stratégies de unsubscribe

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,7 +1,8 @@
 import { FaceSnapsService } from '../services/face-snaps.service';
 import { FaceSnap } from './../models/face-snap';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
+import { interval, Subject, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -10,12 +11,23 @@ import { FaceSnapComponent } from '../face-snap/face-snap.component';
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.scss'
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
   public faceSnaps!: FaceSnap[];
+  private _destroy$!: Subject<boolean>;
 
   constructor(private _faceSnapsService: FaceSnapsService) {};
 
   ngOnInit(): void {
+    this._destroy$ = new Subject<boolean>();
     this.faceSnaps = this._faceSnapsService.getFaceSnaps();
+
+    interval(1000).pipe(
+      tap(console.log),
+      takeUntil(this._destroy$)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next(true);
   }
 }
