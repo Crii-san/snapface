a2a74ecb48b3b8df974c4a99a329088f6338bfee
Christelle Souka <soukachristelle@gmail.com>
2025-03-15 16:55:37 +0100

    Créez un formulaire basique avec les template forms

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
+
+  public userEmail: string = 'test@test.fr';
 
   constructor(private router: Router){};
 
+  ngOnInit(): void {
+    
+  }
+
   public onContinue(): void {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  public onSubmitForm(form: NgForm) {
+    console.log(form.value);
   }
 }
